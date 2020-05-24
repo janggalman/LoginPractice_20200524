53ed9411a8f71ccfd69aef5bc6330584f0a45c91
janggalman <janggalman@gmail.com>
2020-05-24 11:25:33 +0900

    아이디/비번 입력값 받어서 로그로 출력

--- app/src/main/java/kr/tjoeun/loginpractice_20200524/MainActivity.kt
+++ app/src/main/java/kr/tjoeun/loginpractice_20200524/MainActivity.kt
@@ -2,11 +2,25 @@ package kr.tjoeun.loginpractice_20200524
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginBtn.setOnClickListener {
+
+            val inputId = idEdt.text.toString()
+            val inputPw = pwEdt.text.toString()
+
+            Log.d("입력아이디" , inputId)
+            Log.d("입력비번", inputPw)
+
+        }
+
+
     }
 }
